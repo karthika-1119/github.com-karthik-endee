e3cf035dcbb2dac4d50652295e8ec740a57d2fb4
karthika-1119 <125635312+karthika-1119@users.noreply.github.com>
2026-03-12 16:28:09 +0530

    Update ingest.py

--- cyber-ai-assistant/ingest.py
+++ cyber-ai-assistant/ingest.py
@@ -1,1 +1,22 @@
-ingest.py
+from sentence_transformers import SentenceTransformer
+import json
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+with open("cybersecurity.txt") as f:
+    docs = f.readlines()
+
+embeddings = model.encode(docs)
+
+data = []
+
+for i, doc in enumerate(docs):
+    data.append({
+        "text": doc.strip(),
+        "vector": embeddings[i].tolist()
+    })
+
+with open("vectors.json", "w") as f:
+    json.dump(data, f)
+
+print("Embeddings created successfully")
